9daf485237b106a170f784df1ee87cca73f7df18
Terthann <60939529+Terthann@users.noreply.github.com>
2020-04-26 18:08:32 -0700

    Created Card & Region Card Objects

---
+++ Unity/Conspiracies of Runeterra/Assets/Scripts/Card.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    // Public Variables
+    public Sprite back;
+
+    // Private Variables
+    private int cardID;
+    private SpriteRenderer mySprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
